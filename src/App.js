c26d66f28ddb10d16067d6055e73976545b6f1d0
ybbt <44514113+ybbt@users.noreply.github.com>
2020-03-22 17:01:47 +0200

    h1
    delete h1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>By Olexiy.k</h2>
-      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
